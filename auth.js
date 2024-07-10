ce2b8d5555cf3104a0248527d0dbc7bf695b2c79
HO-Inventory <kenshinit2024@gmail.com>
2024-07-10 17:00:20 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -59,10 +59,4 @@ function logout(){
     location.replace("index.html");
 }
 
-firebase.auth().onAuthStateChanged((user)=>{
-    if(!user){
-        location.replace("index.html")
-    }else{
-        document.getElementById("user").innerHTML = "Hello, "+user.email
-    }
-})
+
